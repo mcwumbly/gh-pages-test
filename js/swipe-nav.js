0d1926f4705aceadf6c6af311a1e0e884082616d
David McClure <dave@xerotrope.org>
2015-05-28 21:46:22 -0700

    reverse swipe navigation

--- js/swipe-nav.js
+++ js/swipe-nav.js
@@ -3,10 +3,10 @@ var mc = new Hammer(nav);
 
 // listen to events...
 mc.on("panleft", function(ev) {
-  var link = document.getElementsByClassName('previous-artwork')[0].href
+  var link = document.getElementsByClassName('next-artwork')[0].href
   window.location.href = link; 
 });
 mc.on("panright", function(ev) {
-  var link = document.getElementsByClassName('next-artwork')[0].href
+  var link = document.getElementsByClassName('previous-artwork')[0].href
   window.location.href = link; 
 });
