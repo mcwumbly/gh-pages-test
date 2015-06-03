3e6adf7b918f72d4c68f5ba8a9075ce392d8001a
David McClure <dave@xerotrope.org>
2015-06-02 22:40:50 -0700

    prevent swipe nav while zoomed

--- js/swipe-nav.js
+++ js/swipe-nav.js
@@ -1,12 +1,23 @@
 var nav = document.getElementsByClassName('nav')[0];
 var mc = new Hammer(nav, { touchAction: 'pax-x' } );
 
+var isZoomed = function() {
+  var zoom = document.documentElement.clientWidth / window.innerWidth;
+  return zoom > 1;
+};
+
 // listen to events...
 mc.on("panleft", function(ev) {
+  if (isZoomed()) {
+    return;
+  }
   var link = document.getElementsByClassName('next-artwork')[0].href
   window.location.href = link;
 });
 mc.on("panright", function(ev) {
+  if (isZoomed()) {
+    return;
+  }
   var link = document.getElementsByClassName('previous-artwork')[0].href
   window.location.href = link;
 });
