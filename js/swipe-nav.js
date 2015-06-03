fff68bbad86472202aec8771eeab0312ca56a15f
David McClure <dave@xerotrope.org>
2015-06-02 22:10:24 -0700

    only capture pan-x events on artwork

--- js/swipe-nav.js
+++ js/swipe-nav.js
@@ -1,12 +1,12 @@
 var nav = document.getElementsByClassName('nav')[0];
-var mc = new Hammer(nav);
+var mc = new Hammer(nav, { touchAction: 'pax-x' } );
 
 // listen to events...
 mc.on("panleft", function(ev) {
   var link = document.getElementsByClassName('next-artwork')[0].href
-  window.location.href = link; 
+  window.location.href = link;
 });
 mc.on("panright", function(ev) {
   var link = document.getElementsByClassName('previous-artwork')[0].href
-  window.location.href = link; 
+  window.location.href = link;
 });
